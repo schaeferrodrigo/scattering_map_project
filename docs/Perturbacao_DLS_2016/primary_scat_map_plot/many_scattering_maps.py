759c7797b205f8533f0f378ad9591b2024713e25
rodrigo <schaeferrodrigo@gmail.com>
2018-02-04 18:41:08 +0100

    scat with poincare section

--- docs/Perturbacao_DLS_2016/primary_scat_map_plot/many_scattering_maps.py
+++ docs/Perturbacao_DLS_2016/primary_scat_map_plot/many_scattering_maps.py
@@ -10,14 +10,14 @@ from met_tau import *
 from ponto_fixo import *
 from scattering_map_function import *
 #===============================================================================
-def many_scattering_maps( domain_I_1 , domain_I_2 , domain_theta_1 , domain_theta_2  ):
-    "iteraçao para gerar varias órbitas de um scattering map"
+def many_scattering_maps( domain_I_1 , domain_I_2 , domain_theta_1 , domain_theta_2 , fun ):
+    "iteraçao para gerar varias órbitas de um scattering map , fun assumes the values SM (scattering map) or SM_PS ( scattering map with a poincaré section)"
     dic_I_1  , dic_I_2 , dic_theta_1 , dic_theta_2 = {}  , {} , {} , {}
     for init_I_1 in domain_I_1:
         for init_I_2 in domain_I_2:
             for init_theta_1 in domain_theta_1:
                 for in_theta_2 in domain_theta_2:
                     key =  str(init_I_1)+str(init_I_2)+str(init_theta_1)+str(in_theta_2)
-                    I_1_values , I_2_values , theta_1_values , theta_2_values = SM(init_I_1 , init_I_2, init_theta_1 , in_theta_2 )
+                    I_1_values , I_2_values , theta_1_values , theta_2_values = fun(init_I_1 , init_I_2, init_theta_1 , in_theta_2 )
                     dic_I_1[key] , dic_I_2[key] , dic_theta_1[key] , dic_theta_2[key] = I_1_values, I_2_values, theta_1_values , theta_2_values
     return np.array([dic_I_1 , dic_I_2 , dic_theta_1 , dic_theta_2])
--- docs/Perturbacao_DLS_2016/primary_scat_map_plot/plot_sm.py
+++ docs/Perturbacao_DLS_2016/primary_scat_map_plot/plot_sm.py
@@ -10,10 +10,11 @@ from met_tau import *
 from ponto_fixo import *
 from scattering_map_function import *
 from many_scattering_maps import *
+from plot_sm import *
 import matplotlib.cm as cm
 #===============================================================================
-def plot_scattering_map(domain_I_1, domain_I_2 , domain_theta_1 , domain_theta_2 ):
-    "plot de varias órbitas de Scattering map"
+def plot_scattering_map(domain_I_1, domain_I_2 , domain_theta_1 , domain_theta_2 , fun ):
+    "plot de varias órbitas de Scattering map, fun = function to be plot = many_scattering_maps or many_orbits_poin_sect "
     I_1 , I_2 , theta_1 , theta_2 = many_scattering_maps(domain_I_1 , domain_I_2 ,domain_theta_1, domain_theta_2)
     keys = dict.keys(I_1)
     colors = cm.rainbow(np.linspace( 0,1 , len(keys) ))
@@ -40,4 +41,4 @@ def plot_scattering_map(domain_I_1, domain_I_2 , domain_theta_1 , domain_theta_2
         plt.plot( I_2[key] , I_1[key]  , '.' , color = colors[ index ] , markersize = 0.7)
     plt.show()
 
-plot_scattering_map(domain_I_1 , domain_I_2 , domain_theta_1, domain_theta_2 )
+plot_scattering_map(domain_I_1 , domain_I_2 , domain_theta_1, domain_theta_2 , many_orbits_poin_sect )
