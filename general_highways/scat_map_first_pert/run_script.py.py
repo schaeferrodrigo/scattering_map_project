c9a45a947a7460f71f90bc3198726f17a3889d16
Rodrigo <rodrigo@emp-50-21.eduroam.uu.se>
2020-07-15 12:03:38 +0200

    to move codes about highways for a new repository

--- general_highways/scat_map_first_pert/run_script.py.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-#===============================================================================
-import matplotlib.pyplot as plt
-from scat_map import *
-
-SM(domain_I, domain_theta , step_1)
-plt.show()
